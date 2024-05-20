e8699e417f13e865541ff7bd3ceb40ff36ecece1
Fernanda Bagus <fernandabagus42443@gmail.com>
2024-05-20 11:13:23 +0700

    fern

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/', function () {
+Route::get('/awa', function () {
     return view('hello word');
 });
