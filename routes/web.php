e45dfdf8459be9d6def2d956b3f16462b4630451
Valentino <ellinsuryon@gmail.com>
2024-05-25 09:11:10 +0700

    ubah dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Auth::routes();
+Auth::routes();
 
 
 Route::get('/', function () {
@@ -30,3 +31,14 @@ Route::get('/halo', function () {
 Route::get('/awa', function () {
     return('hello word');
 });
+Route::get('/dashboard', function () {
+    return view('layouts.master');
+})->middleware(['auth', 'verified'])->name('layouts.master');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
