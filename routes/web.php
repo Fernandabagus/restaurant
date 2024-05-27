77077d6e77c4af59a66f8a8cc1edada8a938f4fe
nkFathoni <nurkholidfathoni@gmail.com>
2024-05-27 11:14:00 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\MenuController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,7 +30,16 @@ Route::get('/awa', function () {
 });
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home' , [DashboardController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+Route::get('/service', [ServiceController::class, 'index'])->name('service.index');
+Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+
+// Route::resource('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::resource('dashboard', DashboardController::class);
 
 Route::get('/tony', function () {
     return view('branch tony');
