20c094e48346333effdeb0e38ec7dcd345d2fb5d
Fernanda Bagus <fernandabagus42443@gmail.com>
2024-06-08 19:08:29 +0700

    halaman profile

---
+++ resources/views/myprofile/edit.blade.php
@@ -0,0 +1,45 @@
+<!-- resources/views/myprofile/edit.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="my-4">Edit Profile</h1>
+
+    <form action="{{ route('myprofile.update') }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="form-group mb-3">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}" required>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $user->email) }}" required>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" class="form-control" value="{{ old('username', $user->username) }}" required>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="phone">Phone</label>
+            <input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone', $user->phone) }}" required>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="password">Password (leave blank to keep current password)</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="password_confirmation">Confirm Password</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Update Profile</button>
+    </form>
+</div>
+@endsection
