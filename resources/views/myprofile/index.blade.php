865b70a9d967cfc7c80fe773c712fc36b967420a
Fernanda Bagus <fernandabagus42443@gmail.com>
2024-06-09 11:10:15 +0700

    new profile

--- resources/views/myprofile/index.blade.php
+++ resources/views/myprofile/index.blade.php
@@ -1,47 +1,26 @@
-<!-- resources/views/myprofile/index.blade.php -->
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <h1 class="my-4">My Profile</h1>
-    
+    <h1>My Profile</h1>
     <div class="card">
         <div class="card-body">
-            @if ($user->img)
-                <img src="{{ asset('storage/img' . $user->img) }}" class="img-thumbnail mb-3" alt="img" style="max-width: 150px;">
-            @else
-                <img src="{{ asset('images/default-profile.png') }}" class="img-thumbnail mb-3" alt="Default img" style="max-width: 150px;">
+        @if ($user->img)
+                <img src="{{ asset('storage/' . $user->img) }}" alt="Profile Photo" width="150">
             @endif
+            <h5 class="card-title">{{ $user->name }}</h5>
+            <p class="card-text"><strong>Email:</strong> {{ $user->email }}</p>
+            <p class="card-text"><strong>Username:</strong> {{ $user->username }}</p>
+            <p class="card-text"><strong>Phone:</strong> {{ $user->phone }}</p>
+            <p class="card-text"><strong>Address:</strong> {{ $user->address }}</p>
+           
+            <a href="{{ route('myprofile.edit') }}" class="btn btn-primary">Edit Profile</a>
 
-            <table class="table table-bordered">
-                <tr>
-                    <th>Name</th>
-                    <td>{{ $user->name }}</td>
-                </tr>
-                <tr>
-                    <th>Email</th>
-                    <td>{{ $user->email }}</td>
-                </tr>
-                <tr>
-                    <th>Username</th>
-                    <td>{{ $user->username }}</td>
-                </tr>
-                <tr>
-                    <th>Phone</th>
-                    <td>{{ $user->phone }}</td>
-                </tr>
-                <tr>
-                    <th>Address</th>
-                    <td>{{ $user->address }}</td>
-                </tr>
-            </table>
-
-            <a href="{{ route('myprofile.edit') }}" class="btn btn-primary mt-3">Edit Profile</a>
-
-            <form action="{{ route('myprofile.destroy') }}" method="POST" class="d-inline">
+            <!-- Form for deleting the account -->
+            <form action="{{ route('myprofile.destroy') }}" method="POST" style="display:inline;">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="btn btn-danger mt-3" onclick="return confirm('Are you sure you want to delete your account?');">Delete Account</button>
+                <button type="submit" class="btn btn-danger">Delete Account</button>
             </form>
         </div>
     </div>
