83e87c6a5d1fdafa8e8294fd69ae2093a5fc7a34
Valentino <ellinsuryon@gmail.com>
2024-06-13 22:06:11 +0700

    landing page home

--- resources/views/users/includes/navbar.blade.php
+++ resources/views/users/includes/navbar.blade.php
@@ -112,6 +112,73 @@
       </div>
     </header>
     <!-- slider section -->
+
+    <!-- slider section -->
+    <section class="slider_section">
+        <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" style="background-image: url('image/slider/slider-1.jpg');">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7 col-lg-6">
+                                <div class="detail-box">
+                                    <h1>Family Restaurant</h1>
+                                    <p>
+                                    Stylish cafe serving Western & Asian fare in a contemporary building shaped like a barn.
+                                    </p>
+                                    <div class="btn-box">
+                                        <a href="" class="btn1">Order Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" style="background-image: url('image/slider/slider-2.jpg');">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7 col-lg-6">
+                                <div class="detail-box">
+                                    <h1>Family Restaurant</h1>
+                                    <p>
+                                    Stylish cafe serving Western & Asian fare in a contemporary building shaped like a barn.
+                                    </p>
+                                    <div class="btn-box">
+                                        <a href="" class="btn1">Order Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" style="background-image: url('image/slider/slider-3.jpg');">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7 col-lg-6">
+                                <div class="detail-box">
+                                    <h1>Family Restaurant</h1>
+                                    <p>
+                                    Stylish cafe serving Western & Asian fare in a contemporary building shaped like a barn.
+                                    </p>
+                                    <div class="btn-box">
+                                        <a href="" class="btn1">Order Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <ol class="carousel-indicators">
+                    <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+                    <li data-target="#customCarousel1" data-slide-to="1"></li>
+                    <li data-target="#customCarousel1" data-slide-to="2"></li>
+                </ol>
+            </div>
+        </div>
+    </section>
+    <!-- end slider section -->
     
 
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Users\FoodUsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DrinksController;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\WebController;
