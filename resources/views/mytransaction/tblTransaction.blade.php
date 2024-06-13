f5929c515e28c97ef9e5521db00bd9b97691e0d0
Dedi2312 <dediputrawan2312@gmail.com>
2024-06-13 22:34:30 +0800

    tblTransaction

---
+++ resources/views/mytransaction/tblTransaction.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.master')
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Transaction List</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+              <li class="breadcrumb-item active">Transaction</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container mt-5">
+                <div class="card">
+                    <!-- <div class="card-header text-right">
+                        <a href="{{ route('createFoods') }}" class="btn btn-primary" role="button">Add</a>
+                        <a href="{{ route('trashFoods') }}" class="btn btn-primary" role="button">Trash</a>
+                    </div> -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-hover table-bordered" id="data-table">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Id Transaction</th>
+                                    <th>Id Order</th>
+                                    <th>Transaction Date</th>
+                                    <th>Total Price</th>
+                                    <th>Payment Method</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              <tr class="bg-dark">
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                              </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <script>
+        $(document).ready(function() {
+            $('#food-table').DataTable();
+        });
+    </script>
+@endsection
