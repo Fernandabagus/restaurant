e0716a581a3f084a4605cc312cef4300bf6bf408
Fernanda Bagus <168154862+Fernandabagus@users.noreply.github.com>
2024-06-20 23:22:31 +0700

    Update index.blade.php

--- resources/views/booktable/index.blade.php
+++ resources/views/booktable/index.blade.php
@@ -1,1 +1,109 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Booking List</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Book A Table</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container mt-5">
+                <div class="card">
+                    <!-- <div class="card-header text-right">
+                        <a href="{{ route('booking.trash') }}" class="btn btn-primary" role="button">
+                            <i class="bi bi-recycle"></i> Trash
+                        </a>
+                    </div> -->
+                    <div class="card-body">
+                    <table id="example2" class="table table-hover table-bordered" id="data-table">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Name</th>
+                                    <th>Phone</th>
+                                    <th>Email</th>
+                                    <th>Many Person</th>
+                                    <th>Book Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($booking as $item)
+                                    <tr class="bg-dark">
+                                        <td>{{ $loop->index + 1 }}</td>
+                                        <td>{{ $item->user->name}}</td>
+                                        <td>{{ $item->user->phone}}</td>
+                                        <td>{{ $item->user->email}}</td>
+                                        <td>{{ $item->many_person}}</td>
+                                        <td>{{ $item->book_date}}</td>
+                                        <td>
+                                                <!-- Button trigger modal -->
+                                                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal{{ $loop->index }}">
+                                                    Delete
+                                                </button>
+
+                                                <!-- Modal -->
+                                                <div class="modal fade" id="exampleModal{{ $loop->index }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">DELETE</h5>
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <p>Are you sure want to delete this data?</p>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+                                                                <form action="{{ route('booking.delete', ['booking' => $item->id]) }}" method="POST">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <button type="submit" class="btn btn-danger btn-sm" role="button">Hapus</button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="6" class="text-center">
+                                            <div class="alert alert-danger">There are no book a table.</div>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Initialize DataTables -->
+    <script>
+        $(document).ready(function() {
+            $('#data-table').DataTable();
+        });
+    </script>
+@endsection
 
